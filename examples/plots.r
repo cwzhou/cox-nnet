8158c66d807bf3c645c51571ce1fb689ca7f562c
cwzhou <31316236+cwzhou@users.noreply.github.com>
2021-03-02 15:19:08 -0500

    Update plots.r

--- examples/plots.r
+++ examples/plots.r
@@ -20,45 +20,21 @@ plotCoxMLP <- function(pred, time_test, event_test, png, title) {
 }
 
 ## Plots for PBC
-read.csv("PBC_cv_likelihoods.csv", header=F) -> cvll
+read.csv("Comparison/cox-nnet/results/WHAS_likelihoods.csv", header=F) -> cvll
 L2 <- seq(-4.5,0.5,0.5)
 se <- function(x) sd(x)/sqrt(length(x))
 se <- apply(cvll, 1, se)
 mean <- apply(cvll, 1, mean)
 data <- data.frame(L2, mean, se)
-png("PBC_cv_likelihoods.png", width = 360, height = 360)
+png("WHAS_PBC_cv_likelihoods.png", width = 360, height = 360)
 g <- ggplot(data, aes(x=L2, y=mean)) + 
     geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1) +
     geom_line() +
-    geom_point() + labs(title = "PBC CV log likelihoods vs. L2 parameter")
+    geom_point() + labs(title = "WHAS CV log likelihoods vs. L2 parameter")
 print(g)
 dev.off()
 
-as.numeric(readLines("PBC_ystatus_test.csv")) -> event_test
-as.numeric(readLines("PBC_ytime_test.csv")) -> time_test
-readLines("PBC_theta.csv") -> pred
-plotCoxMLP(pred, time_test, event_test, "PBC_survival_curves.png", "PBC median split")
-
-
-## Plots for KIRC
-
-
-read.csv("KIRC_cindex.csv", header=F) -> cvll
-L2 <- seq(-3,1.67,0.33)
-
-se <- function(x) sd(x)/sqrt(length(x))
-se <- apply(cvll, 1, se)
-mean <- apply(cvll, 1, mean)
-data <- data.frame(L2, mean, se)
-png("KIRC_cindex.png", width = 360, height = 360)
-g <- ggplot(data, aes(x=L2, y=mean)) + 
-    geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=.1) +
-    geom_line() +
-    geom_point() + labs(title = "KIRC C-Index vs. L2 parameter")
-print(g)
-dev.off()
-
-as.numeric(readLines("KIRC_ystatus_test.csv")) -> event_test
-as.numeric(readLines("KIRC_ytime_test.csv")) -> time_test
-readLines("KIRC_theta.csv") -> pred
-plotCoxMLP(pred, time_test, event_test, "KIRC_survival_curves.png", "KIRC median split")
+as.numeric(readLines("Comparison/cox-nnet/results/WHAS_ystatus_test.csv")) -> event_test
+as.numeric(readLines("Comparison/cox-nnet/results/WHAS_ytime_test.csv")) -> time_test
+readLines("Comparison/cox-nnet/results/WHAS_theta.csv") -> pred
+plotCoxMLP(pred, time_test, event_test, "WHAS_PBC_survival_curves.png", "WHAS median split")
